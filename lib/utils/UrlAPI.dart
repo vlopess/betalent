f06fbd6ce193e0133eaeb96183cc9cf58811a967
victor <victorlopes9291@gmail.com>
2024-11-27 20:18:05 -0300

    fix: ajuste em UrlAPI

--- lib/utils/UrlAPI.dart
+++ lib/utils/UrlAPI.dart
@@ -1,6 +1,8 @@
 
 
 abstract class UrlAPI {
-  static String get _partialPath => "http://192.168.10.14:3000";
+  //mudar para o IP da máquina
+  static const String _ip = "192.168.10.14";
+  static String get _partialPath => "http://$_ip:3000";
   static String get listarEmployees => "$_partialPath/employees";
 }
